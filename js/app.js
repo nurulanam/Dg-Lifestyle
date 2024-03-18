511d27132e1a66535ebf0bac28409c2c5fb79c84
nurulanam <nurulanam.bd@gmail.com>
2024-03-18 15:13:11 +0600

    Fix: Home Page Responsiveness

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function ($) {
-    const swiper = new Swiper('.swiper.swiper-categories', {
-        slidesPerView: 1,
-        spaceBetween: 30,
+    const swiper1 = new Swiper('.swiper.swiper-categories1', {
+        slidesPerView: 2,
+        spaceBetween: 10,
         direction: 'horizontal',
         loop: true,
-        // autoplay: {
-        //     delay: 2500,
-        //     disableOnInteraction: false,
-        //   },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
         breakpoints: {
             576: {
               slidesPerView: 2,
@@ -15,19 +15,56 @@ jQuery(document).ready(function ($) {
             },
             768: {
               slidesPerView: 2,
-              spaceBetween: 40,
+              spaceBetween: 20,
             },
             992: {
               slidesPerView: 3,
-              spaceBetween: 30,
+              spaceBetween: 20,
             },
             1200: {
                 slidesPerView: 4,
-                spaceBetween: 80,
+                spaceBetween: 20,
             },
             1600: {
                 slidesPerView: 5,
-                spaceBetween: 80,
+                spaceBetween: 20,
+            }
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      });
+      const swiper2 = new Swiper('.swiper.swiper-categories2', {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+            reverseDirection: true,
+          },
+        breakpoints: {
+            576: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1600: {
+                slidesPerView: 5,
+                spaceBetween: 20,
             }
         },
         navigation: {
